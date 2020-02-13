b11de3c1acd8acd02603762f943f60ad365b2327
10h137 <kwm5@st-andrews.ac.uk>
2020-02-13 14:46:34 +0000

    first commit

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class A2Main {
+public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         if (args.length < 2 || args.length > 3) return;
